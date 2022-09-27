6bfda71e8f83f344678e4fa99f6ae0881736c5b8
Ravneet Singh <ravneetsingh@Ravneets-MacBook-Air.local>
2022-09-26 20:30:31 -0400

    added nav bar with mobile friendly design

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
+
+
+
+
+const NavBar = () => {
+
+const [nav, setNav] = useState(false)
+
+const links = [
+    {
+    id:1,
+    link:"Home"
+    },
+    {
+        id:2,
+        link:'About'
+    },
+    {
+        id:3,
+        link:'Portfolio'
+    },
+    {
+        id:4,
+        link:'Experience'
+    },
+    {
+        id:5,
+        link:'Contact'
+    }
+
+]
+
+
+
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed" >
+    <div>
+        <h1 className='text-5xl font-signature ml-2'>Ravneet</h1>
+    </div>
+    <ul className='hidden md:flex'>
+
+    {links.map(linkProp => (
+        <li key={linkProp.id}
+        className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-300"
+        
+        >
+        {linkProp.link}
+
+
+        </li>
+    ))}
+
+        
+    </ul>
+    <div onClick={()=> setNav(!nav)} className='cursor-pointer p-4 z-10 text-gray-600 md:hidden'>
+   
+   {nav ? <FaTimes size={30} /> : <FaBars size={30} /> }
+
+   {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 ">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+        
+    </div>
+   
+    
+    </div>
+  );
+}
+
+export default NavBar;
