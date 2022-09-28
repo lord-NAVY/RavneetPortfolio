5146a71446b4a66b80726b7c6e925648d486cb02
Ravneet Singh <ravneetsingh@Ravneets-MacBook-Air.local>
2022-09-28 17:43:36 -0400

    updated most part with react and tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Experience from "./Components/Experience";
+import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
+import Portfolio from "./Components/Portfolio";
+import SocialLinks from "./Components/SocialLinks";
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
+      <SocialLinks />
+      <Experience />
+      <Contact />
 
     </div>
   );
